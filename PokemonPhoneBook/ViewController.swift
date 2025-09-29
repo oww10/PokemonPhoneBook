e19e6ad514f7c8b7bd5195135cb0e1a20111c825
oww10 <dntjr2209@gmail.com>
2025-09-29 11:27:07 +0900

    Initial Commit

---
+++ PokemonPhoneBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokemonPhoneBook
+//
+//  Created by oww on 9/29/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
